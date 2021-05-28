d7f5af11a6cc84e68883c14a417a9d6012bc6298
ravi maithrey <maithrey.ind@gmail.com>
2021-05-29 00:45:24 +0530

    added colours and option to give a custom catfile

--- catsay/src/main.rs
+++ catsay/src/main.rs
@@ -1,7 +1,9 @@
 extern crate structopt; //using an external crate
+extern crate colored; //using colours in terminal
+
 
 use structopt::StructOpt; 
-
+use colored::*;
 
 #[derive(StructOpt)]
 struct Options{
@@ -10,16 +12,36 @@ struct Options{
 
     #[structopt(short = "d", long = "dead")] /// Make the cat appear dead with x'd out eyes
     dead: bool,
+
+    #[structopt(short = "f", long = "file", parse(from_os_str))] /// Read cat picture from a file
+    catfile: Option<std::path::PathBuf>,
 }
 
 fn main() {
     let options = Options::from_args(); // calling the args from the Options struct defined above
     let message = options.message; // calling the arg names message from the stuct called above
     let eye = if options.dead {"x"} else {"o"};
-    println!("{}", message);
-    println!(" \\");
-    println!("  \\");
-    println!("      /\\_/\\");
-    println!("      ( {eye} {eye} )", eye=eye);
-    println!("      =( I )=");
+
+    match &options.catfile{
+        Some (path) =>
+        {
+            let cat_template = std::fs::read_to_string(path)
+                                    .expect(&format!("could not read file {:?}", path));
+            let cat_picture = cat_template.replace("{eye}", eye);
+            println!("{}", cat_picture);
+        },
+    
+        None => {
+            if message.to_lowercase() == "woof"{
+                eprintln!("It's a cat, not a dog!");
+            }
+
+            println!("{}", message.yellow().on_blue());
+            println!(" \\");
+            println!("  \\");
+            println!("     /\\_/\\");
+            println!("     ( {eye} {eye} )", eye=eye.red().bold());
+            println!("     =( I )=");
+        }
+    }
 }
