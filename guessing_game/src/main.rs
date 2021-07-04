422d96eab0c397606c6c85da38817b0ff33ab8fb
ravi maithrey <maitrey.ind@gmail.com>
2021-07-04 19:34:35 +0530

    a way to compare guesses

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,6 +1,12 @@
+use rand::Rng;
 use std::io;
+use std::cmp::Ordering;
 
 fn main() {
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    println!("The secret number is {}", secret_number);
+
     println!("Guess the number!");
     println!("Please input your guess here - ");
 
@@ -9,5 +15,13 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
     println!("Your guess was: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
